ae47a0f4af6f094d898aa30bfbe39c0ab0bab74f
taeKrits <kritsada@ada-soft.com>
2025-01-27 14:08:07 +0700

    add libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,22 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("maven-publish")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.github.AdaDevDroid" // GitHub Username
+            artifactId = "sunmiui" // ชื่อ Library
+            version = "1.0.0" // เวอร์ชันของ Library
+// ระบุไฟล์ .aar ที่ต้องการเผยแพร่
+            artifact("libs/sunmiui-1.1.27.aar") {
+                extension = "aar" // ระบุชนิดไฟล์
+            }
+        }
+        repositories {
+            mavenLocal()
+        }
+    }
 }
